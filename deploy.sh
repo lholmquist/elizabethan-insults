b505b316e804bff0b9f120840bbe3c55f600fa28
Lucas Holmquist <lholmqui@redhat.com>
2019-12-09 13:10:11 -0500

    chore: have noun get created last

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,9 @@
 
 # ./docker.sh
 
-# Go into the noun service and deploy using Nodeshift
-cd noun
-npm run deploy
-
-# Go up to the project root, not really necesarry
-cd ..
-
 kubectl create deployment adjective --image=lholmquist/insults-adjective:v0.0.1
 kubectl expose deployment adjective --port=8080 --target-port=8080 --type=NodePort
 oc expose service adjective
-
-# kubectl create deployment noun --image=lholmquist/insults-noun:v0.0.1
-# kubectl expose deployment noun --port=8080 --target-port=8080 --type=NodePort
-# oc expose service noun
 
 kubectl create deployment insult --image=lholmquist/insults-insult:v0.0.1
 kubectl expose deployment insult --port=8080 --target-port=8080 --type=NodePort
@@ -24,6 +13,17 @@ oc expose service insult
 kubectl create deployment frontend --image=lholmquist/insults-frontend:v0.0.1
 kubectl expose deployment frontend --port=8080 --target-port=8080 --type=NodePort
 oc expose service frontend
+
+# kubectl create deployment noun --image=lholmquist/insults-noun:v0.0.1
+# kubectl expose deployment noun --port=8080 --target-port=8080 --type=NodePort
+# oc expose service noun
+
+# Go into the noun service and deploy using Nodeshift
+cd noun
+npm run deploy
+
+# Go up to the project root, not really necesarry
+cd ..
 
 export FRONTEND=http://`kubectl get route frontend -o jsonpath='{.spec.host}'`
 
