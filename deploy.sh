68097e133123e2fa3616a9c08bf4392c02e3f492
Lucas Holmquist <lholmqui@redhat.com>
2019-12-09 13:06:30 -0500

    chore: add Route and deploy noun using nodeshift

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,29 @@
 
 # ./docker.sh
 
+# Go into the noun service and deploy using Nodeshift
+cd noun
+npm run deploy
+
+# Go up to the project root, not really necesarry
+cd ..
+
 kubectl create deployment adjective --image=lholmquist/insults-adjective:v0.0.1
 kubectl expose deployment adjective --port=8080 --target-port=8080 --type=NodePort
-# oc expose service adjective
+oc expose service adjective
 
-kubectl create deployment noun --image=lholmquist/insults-noun:v0.0.1
-kubectl expose deployment noun --port=8080 --target-port=8080 --type=NodePort
+# kubectl create deployment noun --image=lholmquist/insults-noun:v0.0.1
+# kubectl expose deployment noun --port=8080 --target-port=8080 --type=NodePort
 # oc expose service noun
 
 kubectl create deployment insult --image=lholmquist/insults-insult:v0.0.1
 kubectl expose deployment insult --port=8080 --target-port=8080 --type=NodePort
-# oc expose service insult
+oc expose service insult
 
 kubectl create deployment frontend --image=lholmquist/insults-frontend:v0.0.1
 kubectl expose deployment frontend --port=8080 --target-port=8080 --type=NodePort
-# oc expose service frontend
+oc expose service frontend
 
-export HOST=http://master.montreal-091f.open.redhat.com
-export PORT=`oc get svc/frontend -o jsonpath='{.spec.ports[0].nodePort}'`
+export FRONTEND=http://`kubectl get route frontend -o jsonpath='{.spec.host}'`
 
-echo "Visit the front end at ${HOST}:${PORT}"
+echo "Visit the front end at ${FRONTEND}"
